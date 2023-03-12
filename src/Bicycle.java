0c7dd23f429fe3ad9d7508bee9f6802136f80123
roman <romanvafin92@gmail.com>
2023-03-12 18:06:49 +0300

    ДЗ на проверку

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,11 +1,8 @@
 //package pro.sky.java.homeworks.course2.lesson1;
 
-public class Bicycle {
+public class Bicycle extends Vehicle {
 
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public Bicycle (String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
 }
---
+++ src/Service.java
@@ -0,0 +1,5 @@
+public interface Service {
+    void check(Bicycle bicycle);
+    void check(Car car);
+    void check(Truck truck);
+}
